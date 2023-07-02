ee27bd7b113ba26e023ccd4992a3faee008b571c
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-07-02 12:24:09 +0800

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ const Navbar = () => {
   return (
     <div className="p-2">
       <div className="flex items-center justify-between">
-        <h1 className="text-lg font-bold text-red-400"> To:You~ </h1>
-        <div className="bg-green-200 text-green-500 rounded-md px-8 text-center flex items-center gap-2 justify-center h-6">
+        <h1 className="text-lg font-bold text-red-400"> Archived. </h1>
+        <div className="bg-red-400 text-white rounded-md px-8 text-center flex items-center gap-2 justify-center h-6">
           <h1 className="text-xs"> search name...</h1>
           <div>
             <BsSearch size="14px"/>
