db804f377d89a7ebd7da5ccb0ec0b2c07c40e346
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-07-02 14:45:22 +0800

    udpate

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -7,8 +7,8 @@ const Headers = () => {
             " Archive of Unsaid Words "
           </h1>
         </div>
-        <div className="">
-          <p className="text-xs">A place where you can be yourself</p>
+        <div className="py-3">
+          <p className="text-xs text-[#8d8d8d] text-center">A place where you can be express yourself and tell someone something for a long time </p>
         </div>
       </div>
     </div>
