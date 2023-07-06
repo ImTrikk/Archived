bd7c96e6537ab6e1c686729032551206672825d1
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-07-07 00:52:57 +0800

    update buildurl

--- utils/buildUrl.js
+++ utils/buildUrl.js
@@ -1,5 +1,12 @@
+// export const buildUrl = (path) => {
+//   return import.meta.env.DEV
+//     ? `http://localhost:3001/api${path}`
+//     : `/api${path}`;
+// };
+
+
 export const buildUrl = (path) => {
-  return import.meta.env.DEV
-    ? `http://localhost:3001/api${path}`
-    : `/api${path}`;
+  const isDev = true; // Set this variable manually to indicate the environment
+
+  return isDev ? `http://localhost:3001/api${path}` : `/api${path}`;
 };
