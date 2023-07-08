4c5fe45cea80c5686082c834e174c529bb96e58a
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-07-08 15:17:10 +0800

    removed timout

--- api/controllers/messagesQuery.js
+++ api/controllers/messagesQuery.js
@@ -2,7 +2,7 @@ import { MessageModels } from "../Models/Messages.js";
 
 export const Message = async (req, res) => {
   try {
-    const messages = await MessageModels.find().maxTimeMS(8000);
+    const messages = await MessageModels.find();
     return res.status(201).json(messages);
   } catch (err) {
     console.log(err);
