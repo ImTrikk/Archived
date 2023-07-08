61dfd160ccbeaa1b5bba1d23481bbcce7b49f20d
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-07-08 21:08:35 +0800

    update

--- api/server.js
+++ api/server.js
@@ -4,25 +4,25 @@ import cors from "cors";
 import { dbConnection } from "./database.js";
 import { messageRouter } from "./Routes/messages.js";
 
-const app = express();
+  const app = express();
 
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+  app.use(express.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(
+    cors({
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    })
+  );
 
-// API routes
-app.use("/api/message", messageRouter);
+  // API routes
+  app.use("/api/message", messageRouter);
 
-try {
-  dbConnection();
-} catch (err) {
-  console.log("Error Connection");
-}
+  try {
+    dbConnection();
+  } catch (err) {
+    console.log("Error Connection");
+  }
 
-app.listen(3001, () => console.log("Server listening on port 3001"));
+  app.listen(3001, () => console.log("Server listening on port 3001"));
