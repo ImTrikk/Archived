85877aa028b9d7e7474e077bb73a6823faf67f09
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-07-08 14:47:05 +0800

    update

--- api/controllers/messagesQuery.js
+++ api/controllers/messagesQuery.js
@@ -2,12 +2,12 @@ import { MessageModels } from "../Models/Messages.js";
 
 export const Message = async (req, res) => {
   try {
-    
     const messages = await MessageModels.find();
     // const limit = messages.slice(0, 10)
     // return res.status(201).json(limit);
-     return res.status(201).json(messages);
+    return res.status(201).json(messages);
   } catch (err) {
     console.log(err);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 };
