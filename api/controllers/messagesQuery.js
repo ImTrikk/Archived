437d006130a0e988027ca52f49008bef82a8c0a5
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-07-08 15:13:32 +0800

    update

--- api/controllers/messagesQuery.js
+++ api/controllers/messagesQuery.js
@@ -2,11 +2,10 @@ import { MessageModels } from "../Models/Messages.js";
 
 export const Message = async (req, res) => {
   try {
-    
-    const messages = await MessageModels.find();
+    const messages = await MessageModels.find().setTimeout(() => {}, 8000);
     // const limit = messages.slice(0, 10)
     // return res.status(201).json(limit);
-     return res.status(201).json(messages);
+    return res.status(201).json(messages);
   } catch (err) {
     console.log(err);
   }
