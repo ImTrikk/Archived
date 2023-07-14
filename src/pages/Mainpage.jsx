6538ad6b292840d7816530edf4bb5ffc90813ef4
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-07-14 21:29:08 +0800

    update

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -16,7 +16,7 @@ export const MainPage = () => {
 
   const getMessages = async () => {
     try {
-      let response = await fetch(buildUrl("/message/all"), {
+      let response = await fetch(buildUrl("/message/limit"), {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
