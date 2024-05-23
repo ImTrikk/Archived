f96e46749010e604af0b7c83437e583a3de25749
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2024-05-23 22:24:55 +0800

    add zip file

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,9 @@
 # Ensure script stops on first error
 set -e
 
-sudo apt-get update && sudo apt-get upgrade -y
-
 # Configuration variables
 SERVER="192.168.1.18"
 USER="administrator"
-
-sudo mkdir -p /var/www/web
-
 REMOTE_PATH="/var/www/web"
 PORT="22" # Change this to the alternative port if needed
 
@@ -27,14 +22,16 @@ echo "Building the app..."
 npm install
 npm run build
 
+# Ensure the remote directory exists
+echo "Creating remote directory if it does not exist..."
+ssh -p $PORT $USER@$SERVER "mkdir -p $REMOTE_PATH"
 
-# Deploy files to server t
+# Deploy files to server
 echo "Deploying files to server..."
 scp -P $PORT -r build/* $USER@$SERVER:$REMOTE_PATH
 
-
 # Setting permissions on the server
 echo "Setting permissions..."
-ssh -p $PORT $USER@$SERVER 'sudo chown -R www-data:www-data /var/www/&& sudo chmod -R 755 /var/www/'
+ssh -p $PORT $USER@$SERVER "sudo chown -R www-data:www-data $REMOTE_PATH && sudo chmod -R 755 $REMOTE_PATH"
 
 echo "Done!"
